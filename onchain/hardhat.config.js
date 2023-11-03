86c413c58fd53b9f70c5967634588c1cb5bc412a
francisco-leal <francisco@talentprotocol.com>
2023-11-03 20:00:36 +0000

    Add coredao testnet

--- onchain/hardhat.config.js
+++ onchain/hardhat.config.js
@@ -3,6 +3,7 @@ require("@nomicfoundation/hardhat-ethers");
 
 const INFURA_API_KEY = "KEY";
 const SEPOLIA_PRIVATE_KEY = "0x1111111111111111111111111111111111111111111111111111111111111111";
+const COREDAO_PRIVATE_KEY = "0x1111111111111111111111111111111111111111111111111111111111111111";
 
 module.exports = {
   solidity: "0.8.19",
@@ -10,6 +11,10 @@ module.exports = {
     sepolia: {
       url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
       accounts: [SEPOLIA_PRIVATE_KEY]
+    },
+    coreDaoTestnet: {
+      url: "https://rpc.test.btcs.network/",
+      accounts: [COREDAO_PRIVATE_KEY]
     }
   }
 };
