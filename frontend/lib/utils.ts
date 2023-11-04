b185f592001fa50e9c08a75b4ea2c20be6af6a85
francisco-leal <francisco@talentprotocol.com>
2023-11-04 18:09:56 +0000

    Add trading & selling keys

--- frontend/lib/utils.ts
+++ frontend/lib/utils.ts
@@ -4,3 +4,6 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const COMMUNITY_CONTRACT_ADDRESS = "0x664d86CF8D30EEC4169393a70EB3aa796BE85642";
+export const CHAIN_ID = "1115";
