419199bbdf9abc981e5abd28c5ad2c114c523930
francisco-leal <francisco@talentprotocol.com>
2023-11-05 09:38:52 +0000

    Add gnosis config

--- onchain/hardhat.config.js
+++ onchain/hardhat.config.js
@@ -4,6 +4,7 @@ require("@nomicfoundation/hardhat-ethers");
 const INFURA_API_KEY = "KEY";
 const SEPOLIA_PRIVATE_KEY = "0x1111111111111111111111111111111111111111111111111111111111111111";
 const COREDAO_PRIVATE_KEY = "0x1111111111111111111111111111111111111111111111111111111111111111";
+const GNOSIS_PRIVATE_KEY = "0x1111111"
 
 module.exports = {
   solidity: "0.8.19",
@@ -15,6 +16,10 @@ module.exports = {
     coreDaoTestnet: {
       url: "https://rpc.test.btcs.network/",
       accounts: [COREDAO_PRIVATE_KEY]
+    },
+    gnosis: {
+      url: "https://rpc.ankr.com/gnosis",
+      accounts: [GNOSIS_PRIVATE_KEY]
     }
   }
 };
