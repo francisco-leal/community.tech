37fd242f21aab437cc5e56f06166a7bb7eb3c269
francisco-leal <francisco@talentprotocol.com>
2023-11-05 09:43:03 +0000

    Add neonevm config

--- onchain/hardhat.config.js
+++ onchain/hardhat.config.js
@@ -4,7 +4,8 @@ require("@nomicfoundation/hardhat-ethers");
 const INFURA_API_KEY = "KEY";
 const SEPOLIA_PRIVATE_KEY = "0x1111111111111111111111111111111111111111111111111111111111111111";
 const COREDAO_PRIVATE_KEY = "0x1111111111111111111111111111111111111111111111111111111111111111";
-const GNOSIS_PRIVATE_KEY = "0x1111111"
+const GNOSIS_PRIVATE_KEY = "0x1111111111111111111111111111111111111111111111111111111111111111"
+const NEON_PRIVATE_KEY = "0x1111111111111111111111111111111111111111111111111111111111111111";
 
 module.exports = {
   solidity: "0.8.19",
@@ -20,6 +21,10 @@ module.exports = {
     gnosis: {
       url: "https://rpc.ankr.com/gnosis",
       accounts: [GNOSIS_PRIVATE_KEY]
-    }
+    },
+    neontestnet: {
+      url: "https://devnet.neonevm.org",
+      accounts: [NEON_PRIVATE_KEY]
+    },
   }
 };
--- onchain/scripts/mockSomeData.js
+++ onchain/scripts/mockSomeData.js
@@ -4,26 +4,26 @@ const CommunityKeysArtifact = require("../artifacts/contracts/CommunityKeys.sol/
 async function main() {
   const [deployer] = await ethers.getSigners();
 
-  const address = "0x664d86CF8D30EEC4169393a70EB3aa796BE85642";
+  const address = "0x4275854fDEF5EE848a2F9F9e10f17119E285A498";
   const contract = new ethers.Contract(address, CommunityKeysArtifact.abi, deployer);
 
-  // const createTx = await contract.connect(deployer).createCommunity(deployer.address, ethers.parseUnits("0.05", "ether"), "denites");
-  // await createTx.wait();
+  const createTx = await contract.connect(deployer).createCommunity(deployer.address, ethers.parseUnits("0.05", "ether"), "denites");
+  await createTx.wait();
 
-  // let price = await contract.getBuyPriceAfterFee("denites", 1);
-  // const buy1 = await contract.connect(deployer).buyKey("denites", { value: price });
-  // let receipt = await buy1.wait();
-  // console.log("receipt", receipt.hash);
+  let price = await contract.getBuyPriceAfterFee("denites", 1);
+  const buy1 = await contract.connect(deployer).buyKey("denites", { value: price });
+  let receipt = await buy1.wait();
+  console.log("receipt", receipt.hash);
 
-  // price = await contract.getBuyPriceAfterFee("denites", 1);
-  // const buy2 = await contract.connect(deployer).buyKey("denites", { value: price });
-  // receipt = await buy2.wait();
-  // console.log("receipt", receipt.hash);
+  price = await contract.getBuyPriceAfterFee("denites", 1);
+  const buy2 = await contract.connect(deployer).buyKey("denites", { value: price });
+  receipt = await buy2.wait();
+  console.log("receipt", receipt.hash);
 
-  // price = await contract.getBuyPriceAfterFee("denites", 1);
-  // const buy3 = await contract.connect(deployer).buyKey("denites", { value: price });
-  // receipt = await buy3.wait();
-  // console.log("receipt", receipt.hash);
+  price = await contract.getBuyPriceAfterFee("denites", 1);
+  const buy3 = await contract.connect(deployer).buyKey("denites", { value: price });
+  receipt = await buy3.wait();
+  console.log("receipt", receipt.hash);
   
   const sell = await contract.connect(deployer).sellKeys("denites", 1);
   receipt = await sell.wait();
